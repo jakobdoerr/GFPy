361839abf47e2631d6fa85c57d1f7e1d3e2068ab
Jakob Dörr <jakobdorr@c02zn308lvdm.klientdrift.uib.no>
2020-01-20 17:38:03 +0100

    Worked on plotting CTD section

--- CTD_data.py
+++ CTD_data.py
@@ -6,10 +6,12 @@ This module contains functions that read and plot CTD data.
 from seabird.cnv import fCNV
 import gsw
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 import glob
 from scipy.interpolate import interp1d,griddata
 from matplotlib.dates import date2num
+import cmocean
 
 def create_latlon_text(lat,lon):
     '''Creates two strings which contain a text for latitude and longitude
@@ -122,9 +124,12 @@ def CTD_to_grid(CTD,stations,interp_opt= 1,x_type='distance'):
         station_locs - locations of the stations as X units (time or distance)
     '''
 
+    # construct the Z-vector from the max and min depth of the given stations
     maxdepth = np.nanmax([np.nanmax(-CTD[i]['z']) for i in CTD.keys()])
     mindepth = np.nanmin([np.nanmin(-CTD[i]['z']) for i in CTD.keys()])
     Z = np.linspace(mindepth,maxdepth,int(maxdepth-mindepth)+1)
+    
+    # construct the X-vector, either distance or time
     if x_type == 'distance':
         LAT = np.asarray([d['LAT'] for d in CTD.values()])
         LON = np.asarray([d['LON'] for d in CTD.values()])
@@ -132,6 +137,7 @@ def CTD_to_grid(CTD,stations,interp_opt= 1,x_type='distance'):
     elif x_type == 'time':
         X = np.array([date2num(d['datetime']) for d in CTD.values()])
         X = (X - X[0])*24
+        
     # this X vector is where the stations are located, so save that
     station_locs = X
     fields = set([field for field in CTD[stations[0]] 
@@ -179,8 +185,70 @@ def CTD_to_grid(CTD,stations,interp_opt= 1,x_type='distance'):
         
     return fCTD,Z,X,station_locs
     
-            
-def plot_CTD_section(stations,CTD=None,infile=None,section_name=''):
+     
+def contour_section(ax,X,Y,Z,Z2,station_pos=None,cmap='jet',Z2_contours=None,
+                    clabel='',bottom_depth=None,clevels=None,station_text=''):
+    '''Plots a filled contour plot of *Z*, with contourf of *Z2* on top to 
+    the axes *ax*. It also displays the position of stations, if given in
+    *station_pos*, adds labels to the contours of Z2, given in 
+    *Z2_contours*. If no labels are given, it assumes Z2 is density (sigma0) 
+    and adds its own labels. It adds bottom topography if given in *bottom_depth*.
+    Inputs:
+        ax - axes object to plot on
+        X(array) - X-values
+        Y(array) - Y-values
+        Z(array) - the filled contour field
+        Z2(array) - the contour field on top
+        station_pos(list) - the station positions (optional)
+        cmap - the colormap for the filled contours (optional)
+        Z2_contours(list) - the contour label positions for *Z2* (optional)
+        clabel(string) - label to put on the colorbar (optional)
+        bottom_depth(list) - list with bottom depth
+        clevels(list) - list of color levels to use for *Z* (optional)
+        
+    Outputs:
+        ax - the axes object
+    '''
+    # get the labels for the Z2 contours
+    if Z2_contours is None:
+        Z2_contours = np.concatenate([list(range(21,26)),np.arange(25.5,29,0.2)])
+        Z2_contours = [i for i in Z2_contours 
+                        if np.nanmin(Z2) < i < np.nanmax(Z2)]
+    
+    # get the Y-axis limits 
+    y_limits = (0,np.nanmax(Y)+10)
+    
+    # get the color levels
+    if clevels is None:
+        clevels = 20
+        
+    cT = ax.contourf(X,Y,Z,cmap=cmap,levels=clevels) # draw Z
+    cSIG = ax.contour(X,Y,Z2,levels = Z2_contours,
+                       colors='k',linewidths=[1],alpha=0.6) # draw Z2
+    clabels = plt.clabel(cSIG, Z2_contours,fontsize=8,fmt = '%1.1f') # add contour labels
+    [txt.set_bbox(dict(facecolor='white', edgecolor='none', pad=0,alpha=0.6)) for txt in clabels]
+    plt.colorbar(cT,ax = ax,label=clabel,pad=0.01) # add colorbar
+    ax.set_ylim(y_limits)
+    ax.invert_yaxis()
+    
+    # add bathymetry
+    if bottom_depth is not None:
+        ax.fill_between(station_pos,bottom_depth*0+y_limits[1],bottom_depth,
+                     zorder=999,color='gray')
+       
+    ax.xaxis.set_ticks_position('both')
+    ax.yaxis.set_ticks_position('both')
+    # add station ticks
+    if station_pos is not None:
+        for i,pos in enumerate(station_pos):
+            ax.text(pos,0,'v',ha='center',fontweight='bold')
+            if station_text is not '':
+                ax.annotate(station_text+str(i+1),(pos,0),xytext=(0,10),
+                        textcoords='offset points',ha='center')
+    return ax
+    
+def plot_CTD_section(stations,CTD=None,infile=None,
+                     section_name='',cruise_name = ''):
     '''This function plots a CTD section of Temperature and Salinity,
     given CTD data either directly (through *CTD*) or via a file (through)
     *infile*.
@@ -193,6 +261,7 @@ def plot_CTD_section(stations,CTD=None,infile=None,section_name=''):
         infile - string with file (and path) to a numpy file of the CTD data
                  Can also be made with the function readCTD
         section_name - name of the Section, will appear in the plot title
+        cruise_name - name of the Cruis, will also appear in the title
     outputs:
         fig - a handle to the figure
     '''
@@ -213,15 +282,33 @@ def plot_CTD_section(stations,CTD=None,infile=None,section_name=''):
     # select only the given stations in the data
     CTD = {key:CTD[key] for key in CTD.keys() if key in stations}
     
-    # extract the relevant header fields from the CTD
-    
+
+    # extract Bottom Depth    
     BDEPTH = np.asarray([d['BottomDepth'] for d in CTD.values()])
-    
+
     # put the fields (the vector data) on a regular, common pressure grid
     # by interpolating. 
-    fCTD,Z,X,station_locs = CTD_to_grid(CTD,stations,x_type='time',interp_opt=1)
-
-    # FIXME: DO the plot :)
-    return X,Z,fCTD['T']
+    fCTD,Z,X,station_locs = CTD_to_grid(CTD,stations,x_type='distance')
     
-            
+    # plot the figure!
+    fig,[axT,axS] = plt.subplots(2,1,figsize=(8,9))
+    
+    # Temperature
+    axT = contour_section(axT,X,Z,fCTD['T'],fCTD['SIGTH'],
+                          station_pos=station_locs,cmap=cmocean.cm.thermal,
+                          clabel='Temperature [˚C]',bottom_depth=BDEPTH,
+                          station_text=section_name)
+    # Salinity
+    axS = contour_section(axS,X,Z,fCTD['S'],fCTD['SIGTH'],
+                          station_pos=station_locs,cmap=cmocean.cm.haline,
+                          clabel='Salinity [g kg$^{-1}$]',bottom_depth=BDEPTH)
+    
+    # FIXME: Axis labels!
+    # add title
+    fig.suptitle(cruise_name+' Section '+section_name)
+    
+ 
+# Only for testing purposes:
+if __name__ == '__main__':
+    CTD = readCTD('/Users/jakobdorr/Documents/Phd/Teaching/MATLAB_TO_PYTHON_CRUISE2020/2019_Masfjorden/Data/GS2018/','JAKOB') 
+    plot_CTD_section(range(401,410),CTD = CTD,cruise_name='JAKOB',section_name='A')
