86c9ebb62d1b073d06c7bd859a53a395ab94262f
Jakob Dörr <jakobdorr@c02zn308lvdm.klientdrift.uib.no>
2020-01-28 15:00:37 +0100

    changed setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
   url = 'https://github.com/jakobdoerr/GFPy',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/jakobdoerr/GFPy/archive/v0.0.2.tar.gz',   
   keywords = ['oceanography', 'meteorology', 'bergen'],   # Keywords that define your package best
-  install_requires=requirements,
+  install_requires=['matplotlib,'seabird','numpy','scipy','pandas','netCDF4','cartopy'],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
