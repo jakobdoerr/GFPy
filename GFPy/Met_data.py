5741272ec554d5cdd855761d321b6614d6ba62c4
Christiane Duscha <cdu022@gfi3102840.klientdrift.uib.no>
2020-03-18 13:43:27 +0100

    Included functions to read (this years) Hobo, Opus and EasyLog stations

--- GFPy/Met_data.py
+++ GFPy/Met_data.py
@@ -17,54 +17,8 @@ import matplotlib.dates as mdates
 
 
 # =============================================================================
-# Reading section
+# General reading function
 # =============================================================================
-
-def read_single_AWS(filename):
-    """
-    Read meteorological data from a single Autometed weather station (AWS). Depending on the data format
-    
-    Parameters
-    ==========
-    filename: str
-        path and name of file to be read
-        
-    Returns
-    =======
-    station: pandas dataframe
-        dataframe containing all variables observed by AWS
-    variables: dictionary
-        dictionary containing all met variable shortuts and their long names
-    
-    """
-    data_format = filename.split('.')[-1]
-    
-    if data_format == 'dat':
-        station = read_dat_AWS(filename)
-            # Define variable long names and units
-        variables = {'P': 'Pressure [hPa]',
-                    'TA': r'Air Temperature [$^\circ$C]',
-                    'UU': 'Relative Humidity [%]',
-                    'FF': 'Wind Speed [m/s]',
-                    'DD': r'Wind Direction [$^\circ$]',
-                    'RR_01': 'Rain Rate [mm]',
-                    'QSI_01': r'Shortwave Irradiation [W/m$^2$]'}
-    elif data_format == 'nc':
-        station = read_netcdf_AWS(filename)
-        variables = {'P': 'Pressure [hPa]',
-                     'T': r'Air Temperature [$^\circ$C]',
-                     'RH': 'Relative Humidity [%]',
-                     'FF': 'Wind Speed [m/s]',
-                     'DD': r'Wind Direction [$^\circ$]',
-                     'RR': 'Rain Rate [mm]',
-                     'RAD': r'Shortwave Irradiation [W/m$^2$]'}
-    else:
-        print('The data format '+str(data_format)+' is not supported by read_single_AWS')
-        station   = pd.DataFrame()        
-        variables = pd.DataFrame()     
-    
-    return station, variables
-
 def read_netcdf(file):
     """
     Read netcdf datasets (with maximum of 2 dimension variables)
@@ -118,6 +72,58 @@ def read_netcdf(file):
         
     data.close()
     return buffer
+
+# =============================================================================
+# Instrument specific Reading 
+# =============================================================================
+
+# =============================================================================
+# Automated Weather station   
+# =============================================================================
+def read_single_AWS(filename):
+    """
+    Read meteorological data from a single Autometed weather station (AWS). Depending on the data format
+    
+    Parameters
+    ==========
+    filename: str
+        path and name of file to be read
+        
+    Returns
+    =======
+    station: pandas dataframe
+        dataframe containing all variables observed by AWS
+    variables: dictionary
+        dictionary containing all met variable shortuts and their long names
+    
+    """
+    data_format = filename.split('.')[-1]
+    
+    if data_format == 'dat':
+        station = read_dat_AWS(filename)
+            # Define variable long names and units
+        variables = {'P': 'Pressure [hPa]',
+                    'TA': r'Air Temperature [$^\circ$C]',
+                    'UU': 'Relative Humidity [%]',
+                    'FF': 'Wind Speed [m/s]',
+                    'DD': r'Wind Direction [$^\circ$]',
+                    'RR_01': 'Rain Rate [mm]',
+                    'QSI_01': r'Shortwave Irradiation [W/m$^2$]'}
+    elif data_format == 'nc':
+        station = read_netcdf_AWS(filename)
+        variables = {'P': 'Pressure [hPa]',
+                     'T': r'Air Temperature [$^\circ$C]',
+                     'RH': 'Relative Humidity [%]',
+                     'FF': 'Wind Speed [m/s]',
+                     'DD': r'Wind Direction [$^\circ$]',
+                     'RR': 'Rain Rate [mm]',
+                     'RAD': r'Shortwave Irradiation [W/m$^2$]'}
+    else:
+        print('The data format '+str(data_format)+' is not supported by read_single_AWS')
+        station   = pd.DataFrame()        
+        variables = pd.DataFrame()     
+    
+    return station, variables
     
 def read_netcdf_AWS(filename):
     """
@@ -198,7 +204,9 @@ def read_dat_AWS(filename):
 
     return station
 
-
+# =============================================================================
+# Ship Log (Kristine Bonnevie)
+# =============================================================================
 def read_ship_log(filename):
     """
     Read the data logged by Kristine Bonnevie
@@ -248,7 +256,134 @@ def read_ship_log(filename):
     
     return log
 
+# =============================================================================
+# HOBO Raingauges 
+# =============================================================================
+def read_HOBO(filenames):
+    """
+    Function to read the raw HOBO data for all deployed HOBO stations
+    example input (GEOF232-2020): filenames = [path+'HOBO2020_20547.txt', path+'HOBO2020_20853.txt', 
+                                               path+'HOBO2020_21075.txt', path+'HOBO2020_21079.txt',
+                                               path+'HOBO2020_21081.txt',path+'HOBO2020_21083.txt',
+                                               path+'HOBO2020_21084.txt',path+'HOBO2020_21085.txt']
+    Must define a path first
+    
+    Parameters
+    ==========
+    filenames: list
+        list of path and name of files to be read
+        
+    Returns
+    =======
+    station_ID: list
+        list of station ID names (str) read
+    
+    station: dict
+        Dictionary containing pandas DataFrames corresponding to Raw data of each station
+    hourly: dict
+        Dictionary containing pandas DataFrames corresponding to 1 hour rain rate data of each station
+    """
+    # Define dictionary to save all stations
+    station = {}
+    hourly = {}
+    # Read data
+    # Iterate over stations --> save station ID
+    station_ID = []
+    for file in filenames:
+        ID = file[-9:-4]
+        station_ID.append(ID)
+        station[file[-9:-4]] = pd.read_csv(file, header = 1, names = ['Timestamp', 'data'])
+        station[file[-9:-4]]['Event'], station[file[-9:-4]]['Count'] = station[file[-9:-4]].data.str.split('\t',1).str
+        #del station[file[-18:-13]].data
+        station[file[-9:-4]]['Event'] = station[file[-9:-4]]['Event'].astype(int)
+        station[file[-9:-4]]['Count'] = station[file[-9:-4]]['Count'].astype(int)
+        # Get timestamp
+        station[file[-9:-4]].index = pd.to_datetime(station[file[-9:-4]]['Timestamp'].tolist(),
+               format='%m/%d/%y %H:%M:%S')
+        # Reduce dataset to necessary variables
+        station[file[-9:-4]] = station[file[-9:-4]][['Count','Event']]
+        # set the Rain Rate per count (0.2mm)
+        station[file[-9:-4]]['RR'] = 0.2           
+        
+        # Get the hourly rain rate (mm/h)
+        hourly[file[-9:-4]] = station[file[-9:-4]]['RR'].resample('1h').sum()
+        
+    return station_ID, station, hourly
 
+# =============================================================================
+# OPUS Logger
+# =============================================================================
+def read_OPUS(filenames):
+    """
+    Function to read the raw OPUS Data
+    example input (GEOF232-2020): filenames = [path+'OPUS20_2020_Nr010_20200317_160955.csv',
+                                               path+'OPUS20_2020_Nr012_20200317_160549.csv']
+    
+    Must define a path first
+    
+    Parameters
+    ==========
+    filenames: list
+        list of path and name of files to be read
+        
+    Returns
+    =======
+    station: dict
+        Dictionary containing pandas DataFrames corresponding to Raw data of each station, 
+        Station ID names can be acced by station.keys()
+    
+    """
+    # Define dictionary to save all stations
+    station = {}
+    # Read data
+    # Iterate over stations --> save station ID
+    # Define column names
+    cols = ['date and time', 'temperature','relative humidity','pressure']
+    for file in filenames:
+        ID = file[-25:-20]
+        station[str(ID)] = pd.read_csv(file, header = None,names =cols, skiprows = 10, delimiter=';')
+        timestamp = station[str(ID)]['date and time']
+        station[str(ID)].index = pd.to_datetime(timestamp.tolist(), format='%d.%m.%Y %H.%M.%S')
+        del station[str(ID)]['date and time']
+    return station
+
+# =============================================================================
+# EasyLog
+# =============================================================================
+    
+def read_EasyLog(filenames):
+    """
+    Function to read the raw EasyLog Data
+    example input (GEOF232-2020): filenames = [path+'OPUS20_2020_Nr010_20200317_160955.csv',
+                                               path+'OPUS20_2020_Nr012_20200317_160549.csv']
+    
+    Must define a path first
+    
+    Parameters
+    ==========
+    filenames: list
+        list of path and name of files to be read
+        
+    Returns
+    =======
+    station: dict
+        Dictionary containing pandas DataFrames corresponding to Raw data of each station, 
+        Station ID names can be acced by station.keys()
+    
+    """
+    # Define dictionary to save all stations
+    station = {}
+
+    # Read data
+    # Iterate over stations --> save station ID
+    for file in filenames:
+        ID = file[-5:-4]
+        station[str(ID)] = pd.read_csv(file, skiprows=1, 
+                                       usecols=[1,2], 
+                                       names = ['time','temperature'])
+        station[str(ID)].index = pd.to_datetime(station[str(ID)]['time'].tolist(), format='%d/%m/%Y %H:%M:%S')
+        del station[str(ID)]['time']
+    return station
 # =============================================================================
 # Ploting section
 # =============================================================================
